dd0024ac6577abf92554e5e4f420a116e50f4d66
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-11-25 22:31:28 +0800

    Update BardAI.d.ts

--- lib/BardAI.d.ts
+++ lib/BardAI.d.ts
@@ -1,24 +1,40 @@
+// bardAI.d.ts
+
 export = BardAI;
 
 /**
- * @typedef {BardAI} BardAI
- * @example import BardAI from "bardAI";
+ * @typedef {Class} BardAI
+ * @see {BardAI}
+ * @param {Class} BardAI
+ * @example const { BardAI } = require("bardie");
+ * @example import { BardAI } from "bardie";
+ * @type {Class}
+ * @class
  */
 declare class BardAI {
+  constructor();
+
   /**
    * The Question You Want to Ask Artificial Intelligence.
-   * @param {string} ask - The question you want to ask AI.
-   * @example client.question("How are you?");
-   * @returns {Promise<string>} The response from the AI.
+   * @param {Object} options Options for the question.
+   * @param {string} options.ask - The Question You Want to Ask Artificial Intelligence.
+   * @example const bardAI = new BardAI();
+   * bardAI.question({ ask: "Who is Elon Musk?" });
+   * @type {Promise<Object>} The response from the AI.
    */
-  question(ask: string): Promise<string>;
+  question(options: { ask: string }): Promise<Object>;
 
   /**
-   * The Status You want to get from bard.rizzy.eu.org.
-   * @example client.status()
-   * @returns {Promise<string>} The status from bard.rizzy.eu.org.
+   * The Question You Want to Ask Artificial Intelligence.
+   * @param {Object} options Options for the question.
+   * @param {string} options.ask - The Question You Want to Ask Artificial Intelligence.
+   * @param {string} options.image - URL of the image.
+   * @example const bardAI = new BardAI();
+   * bardAI.questionWithImage({ ask: "What is in this image?", image: "https://telegra.ph/file/c34c149838ae6a1768897.jpg" });
+   * @type {Promise<Object>} The response from the AI.
    */
-  status(): Promise<string>;
+  questionWithImage(options: { ask: string, image: string }): Promise<Object>;
 }
 
+// Ensure the namespace is empty if not used
 declare namespace BardAI {}
