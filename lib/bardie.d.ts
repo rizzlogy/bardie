856fea31cd444f6b7d8669b487a5b9aa0236de31
Zoheb Malik <mzohebm@gmail.com>
2023-12-23 22:06:11 +0000

    bardie.d.ts: Refactor Logic
    This immensely simplifies the logic and JSDocs, given that the interfaces are self-explanatory and everything is typed more specifically now.

--- lib/bardie.d.ts
+++ lib/bardie.d.ts
@@ -1,37 +1,26 @@
 export = Bardie;
 
-/**
- * @typedef {Class} Bardie
- * @see {Bardie}
- * @param {Class} Bardie
- * @example const { Bardie } = require("bardie");
- * @example import { Bardie } from "bardie";
- * @type {Class}
- * @class
- */
+interface questionOptions {
+  ask: string;
+  image?: string;
+}
+
+interface AIResponse {
+  content: string;
+  status: boolean;
+  creator: string;
+}
+
 declare class Bardie {
   constructor();
 
   /**
-   * The Question You Want to Ask Artificial Intelligence.
-   * @param {Object} options Options for the question.
-   * @param {string} options.ask - The Question You Want to Ask Artificial Intelligence.
-   * @example const bard = new bardie();
-   * bard.question({ ask: "Who is Elon Musk?" });
-   * @type {Promise<Object>} The response from the AI.
+   * Ask a question to the artificial intelligence.
+   * @param {questionOptions} options - The options for the question.
+   * @returns {Promise<AIResponse>} The response from the AI.
+   * @throws {Error} If the question is not specified or the image URL is invalid.
    */
-  question(options: { ask: string }): Promise<Object>;
-
-  /**
-   * The Question You Want to Ask Artificial Intelligence.
-   * @param {Object} options Options for the question.
-   * @param {string} options.ask - The Question You Want to Ask Artificial Intelligence.
-   * @param {string} options.image - URL of the image.
-   * @example const bard = new bardie();
-   * bard.questionWithImage({ ask: "What is in this image?", image: "https://telegra.ph/file/c34c149838ae6a1768897.jpg" });
-   * @type {Promise<Object>} The response from the AI.
-   */
-  questionWithImage(options: { ask: string, image: string }): Promise<Object>;
+  question(options: questionOptions): Promise<AIResponse>;
 }
 
 declare namespace Bardie {}
