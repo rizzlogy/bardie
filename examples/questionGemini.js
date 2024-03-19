101d1b90c8fa3edb3f6e38658a1bfeda8379fb9a
RizzyFuzz <prodyagiri@gmail.com>
2024-03-19 23:18:02 +0800

    Create questionGemini.js

---
+++ examples/questionGemini.js
@@ -0,0 +1,13 @@
+const { Bardie } = require("../lib/bardie");
+const bard = new Bardie();
+
+async function askQuestion() {
+  try {
+    let result = await bard.questionGemini({ ask: "Keep it simple... What is Google Bard?" });
+    console.log(result);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+askQuestion();
